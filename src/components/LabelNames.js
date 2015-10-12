b4fe799dab1c7f55cc9539f2c29e34428bc1868e
James Shi <jshi@live.ca>
2015-10-12 09:41:21 -0400

    added labels

---
+++ src/components/LabelNames.js
@@ -0,0 +1,13 @@
+'use strict';
+
+var RunstatConstants = require('../constants/RunstatConstants');
+
+var LabelNames = {};
+
+LabelNames[RunstatConstants.KM10] = '10 km';
+LabelNames[RunstatConstants.KM21] = '21 km';
+LabelNames[RunstatConstants.KM30] = '30 km';
+LabelNames[RunstatConstants.KM40] = '40 km';
+LabelNames[RunstatConstants.TIME] = 'Finish Time';
+
+module.exports = LabelNames;
--- src/components/Search.js
+++ src/components/Search.js
@@ -7,6 +7,7 @@ require('normalize.css');
 require('../styles/main.css');
 
 var RunstatActions = require('../actions/RunstatActions');
+var LabelNames = require('./LabelNames');
 
 var Search = React.createClass({
   onChange: function(event) {
@@ -16,6 +17,7 @@ var Search = React.createClass({
   render: function() {
     return (
       <div className={'col-xs-' + this.props.col}>
+        <label>{LabelNames[this.props.stat]}</label>
         <input type='text' className='form-control' onChange={this.onChange} />
       </div>
     );
