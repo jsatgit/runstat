a0966bd0d0388440700aa6435f5df5a87ee1d5ee
James Shi <jshi@live.ca>
2015-10-12 15:03:38 -0400

    removed splash image on stats page

--- src/components/RunstatWebApp.js
+++ src/components/RunstatWebApp.js
@@ -1,6 +1,7 @@
 'use strict';
 
 var React = require('react/addons');
+var $ = require('jquery');
 
 // CSS
 require('normalize.css');
@@ -15,6 +16,10 @@ var Results = require('./Results');
 require('../stores/RunstatStore');
 
 var RunstatWebApp = React.createClass({
+  componentWillMount: function() {
+    $(document.body).removeClass('splash');
+  },
+
   componentDidMount: function() {
     Filter.init();
   },
